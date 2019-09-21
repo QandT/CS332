eb7d09e6a78736dcf07284fe996446d60e8ad5b3
qmb2 <qmb2@gold30.cs.calvin.edu>
2019-09-21 09:56:14 -0400

    Added last comments

--- lab1/TestServer.java
+++ lab1/TestServer.java
@@ -2,6 +2,14 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+/**
+ * This is a test class that creates a server, light display,
+ * and a bit display.
+ * 
+ * @author: Quentin Barnes
+ * @author: Ty Vredeveld
+ */
+
 public class TestServer {
     public static void main(String[] args) {
         LightSystem system = new LightSystem();
