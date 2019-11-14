86cfe5ba0ad2ab9793f8a24378a49efe5f348326
tjv37 <tjv37@gold28.cs.calvin.edu>
2019-11-14 13:22:34 -0500

    base files for RCMP project

---
+++ RCMP/RCMPClient.java
@@ -0,0 +1,13 @@
+/**
+ * This class implements a client that implements RCMP
+ *
+ * @author: Quentin Barnes
+ * @author: Ty Vredeveld
+ */
+public class RCMPClient {
+
+	public RCMPClient() {
+
+	}
+
+}
---
+++ RCMP/RCMPServer.java
@@ -0,0 +1,13 @@
+/**
+ * This class implements a server to test our RCMP client with
+ *
+ * @author: Quentin Barnes
+ * @author: Ty Vredeveld
+ */
+public class RCMPServer {
+
+	public RCMPServer() {
+
+	}
+
+}
